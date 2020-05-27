a0649c7ce0f7490426f1c6c9e18df70ea8b17c54
AhmedElsayed101 <elsayedahmed945@gmail.com>
2020-05-27 11:12:26 +0200

    creating most of the api routes

--- api/apiRoutes.py
+++ api/apiRoutes.py
@@ -1,4 +1,8 @@
-from flask import render_template,jsonify, redirect
+from flask import (render_template,
+                    jsonify,
+                    redirect,abort,
+                    request
+                  )
 from app import app
 from database.models import *
 
@@ -13,6 +17,13 @@ def api():
 def login():
     url = "https://ayat101.auth0.com/authorize?audience=coffee&response_type=token&client_id=72qvFBzX50vyoVDJqc3wQoBsPjpCMBZY&redirect_uri=https://127.0.0.1:8100/tabs/user-page"
     return redirect(url)
+
+# @app.route('/api/actors/new')
+# def add_actor_info():
+#     # get users info from auth0 to store in database  
+#     return jsonify({
+#         "actors":"all actors"
+#     })
     
 @app.route('/logout')
 def logout():
@@ -23,51 +34,192 @@ def logout():
 
 @app.route('/api/actors')
 def get_all_actors():
+
+    actros = Actor.query.order_by(Actor.id).all()  
+
+    if len(actros) == 0:
+        abort(404)
+    
+    actors_formatted = [actor.format() for actor in actros]
+
     return jsonify({
-        "actors":"all actors"
+
+        "success" : True,
+        "actors": actors_formatted,
+        "actors_number": len(actors_formatted)
     })
+
+
 
 @app.route('/api/actors', methods = ['POST'])
 def create_new_actor():
+
+    data = request.get_json()
+    try:
+        actor_name = data['name']
+        actor_age = data['age']
+        actor_gender = data['gender']
+    except:
+        abort(401)
+    
+    new_actor = Actor(
+        name = actor_name,
+        age = actor_age,
+        gender = actor_gender
+    )
+
+    new_actor.insert()
+
     return jsonify({
-        "actors":"all actors"
+        "success" : True,
+        "created" : new_actor.id
     })
 
-@app.route('/api/actors/<int:actor_id>', methods = ['POST'])
-def delete_new_actor(actor_id):
+
+@app.route('/api/actors/<int:actor_id>', methods = ['GET'])
+def get_actor(actor_id):
+
+    current_actor = Actor.query.get(actor_id)
+    if current_actor is None:
+        abort(404)
+    current_actor_formatted = current_actor.format()
     return jsonify({
-        "actors":"all actors"
+        "actor": current_actor_formatted
+    })
+
+@app.route('/api/actors/<int:actor_id>', methods = ['DELETE'])
+def delete_new_actor(actor_id):
+
+    current_actor = Actor.query.get(actor_id)
+    if current_actor is None:
+        abort(404)
+
+    current_actor.delete()
+    return jsonify({
+        "success" : True,
+        "deleted" : actor_id
     })
 
 @app.route('/api/actors/<int:actor_id>', methods = ['PATCH'])
 def edit_new_actor(actor_id):
+
+    data = request.get_json()
+
+    current_actor = Actor.query.get(actor_id)
+
+    try:
+        actor_name = data['name']
+        actor_age = data['age']
+        actor_gender = data['gender']
+
+    except:
+        abort(401)
+    
+   
+    current_actor.name = actor_name,
+    current_actor.age = actor_age,
+    current_actor.gender = actor_gender
+   
+    current_actor.update()
+
     return jsonify({
-        "actors":"all actors"
+        "success" : True,
+        "updated" : current_actor.id
     })
+    
 
 
 
 @app.route('/api/movies')
 def get_all_movies():
+
+    movies = Movie.query.order_by(Movie.id).all()  
+
+    if len(movies) == 0:
+        abort(404)
+    
+    movies_formatted = [movie.format() for movie in movies]
+
+    return jsonify({
+
+        "success" : True,
+        "movies": movies_formatted,
+        "movies_number": len(movies_formatted)
+    })
     return jsonify({
         "actors":"all movies"
     })
 
 @app.route('/api/movies', methods = ['POST'])
 def create_new_movie():
+    
+    data = request.get_json()
+    try:
+        movie_title = data['title']
+        movie_start_time = data['start_time']
+    except:
+        abort(401)
+    
+    new_movie = Movie(
+
+        title = movie_title,
+        start_time = movie_start_time
+    )
+
+    new_movie.insert()
+
     return jsonify({
-        "actors":"all movies"
+        "success" : True,
+        "created" : new_movie.id
     })
 
-@app.route('/api/movies/<int:movie_id>', methods = ['POST'])
-def delete_new_movie(movie_id):
+@app.route('/api/movies/<int:movie_id>', methods = ['GET'])
+def get_movie(movie_id):
+
+    current_movie = Movie.query.get(movie_id)
+    if current_movie is None:
+        abort(404)
+    current_movie_formatted = current_movie.format()
     return jsonify({
-        "actors":"all movies"
+        "movie": current_movie_formatted
+    })
+
+
+@app.route('/api/movies/<int:movie_id>', methods = ['DELETE'])
+def delete_new_movie(movie_id):
+    
+    current_movie = Movie.query.get(movie_id)
+    if current_movie is None:
+        abort(404)
+
+    current_movie.delete()
+    return jsonify({
+        "success" : True,
+        "deleted" : movie_id
     })
 
 @app.route('/api/movies/<int:movie_id>', methods = ['PATCH'])
 def edit_new_movie(movie_id):
+    
+    data = request.get_json()
+
+    current_movie = Movie.query.get(movie_id)
+
+    try:
+        movie_title = data['title']
+        movie_start_time = data['start_time']
+    except:
+        abort(401)
+    
+   
+    current_movie.title = movie_title
+    current_movie.start_time = movie_start_time
+   
+    current_movie.update()
+
     return jsonify({
-        "actors":"all movies"
+        "success" : True,
+        "updated" : current_movie.id
     })
+    
 
--- database/models.py
+++ database/models.py
@@ -2,12 +2,61 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Movies(db.Model):
-#     pass
+class Movie(db.Model):
+
+    __tablename__ = 'movies'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable= False)
+    start_time = db.Column(db.Date, nullable=False)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+  
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'id': self.id,
+        'title': self.title,
+        'start_time': self.start_time
+        }
 
 
 
-# class Actors(db.Model):
-#     pass
+class Actor(db.Model):
+
+    __tablename__ = 'actors'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    # auth0_id = db.Column(db.String(80), nullable = False)
+    name = db.Column(db.String(80), nullable= False)
+    age = db.Column(db.Integer, nullable= False)
+    gender = db.Column(db.String(20), nullable= False)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+  
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'id': self.id,
+        'name': self.name,
+        'age': self.age,
+        'gender': self.gender
+        }
 
 
