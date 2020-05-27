adb3a18bee74c0034d401be42763a42796c56931
AhmedElsayed101 <elsayedahmed945@gmail.com>
2020-05-27 13:15:26 +0200

    fixing setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 export DATABASE_URL=postgres://sjxbcynwitrykr:9b0d26eec9cab38b852ffe953d48046ed2da4c9a49231ebbd0826067efcb45a6@ec2-35-169-254-43.compute-1.amazonaws.com:5432/d76g8a4qhhgarj
 export ALGORITHMS=['RS256']
-export SECRET_KEY='HcipH6LJOlPO4fm4-mvGqixQNxsPh2Pz_GQHApl1uvw'
-export AUTH0_DOMAIN='ayat101.auth0.com'
-export API_AUDIENCE='coffee'
+export SECRET_KEY=HcipH6LJOlPO4fm4-mvGqixQNxsPh2Pz_GQHApl1uvw
+export AUTH0_DOMAIN=ayat101.auth0.com
+export API_AUDIENCE=coffee
 export ASSISTANT_TOKEN=''
 export DIRECTOR_TOKEN=''
 export PRODUCER_TOKEN=''
