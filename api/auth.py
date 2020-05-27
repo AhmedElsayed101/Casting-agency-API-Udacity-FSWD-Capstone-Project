3f43e348a1f9c3cf0ca9e689633963f365ae7ad2
AhmedElsayed101 <elsayedahmed945@gmail.com>
2020-05-27 13:06:50 +0200

    adding env variables

--- api/auth.py
+++ api/auth.py
@@ -3,11 +3,11 @@ from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import os
 
-
-AUTH0_DOMAIN = 'ayat101.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffee'
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 
 class AuthError(Exception):
--- api/init.py
+++ api/init.py
@@ -6,6 +6,8 @@ import os
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL_LOCAL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app=app)
